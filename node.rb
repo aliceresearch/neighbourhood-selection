fdb0db653c0fb69bd910bde6a2a9ef4a09eb965f
Kyrre <kyrrehg@gmail.com>
2011-11-22 16:04:53 +0000

    update of tau values and utility function

--- node.rb
+++ node.rb
@@ -7,6 +7,9 @@ class Node
   def initialize id
     @taus = {}
     @node_id = id
+    @evaporation_rate = 0.1
+    @delta = 1
+    @weights = { :beta => 0.5, :gamma => 0.5 }
   end
   
   def step1 possible_nodes
@@ -26,7 +29,15 @@ class Node
   def step2 benefits_and_costs
     benefits_and_costs.each { |i, values| 
       puts "node_id #{i} benefits_and_costs #{values.inspect}"
+      @taus[i]= (1-@evaporation_rate) * @taus[i]
+      if (utility values) > 0 
+        @taus[i] = @taus[i] + @delta
+      end
     }
+  end
+  
+  def utility values
+    @weights[:beta] * values[:beta] - @weights[:gamma] * values[:gamma]
   end
 
   def print_taus
--- simulator.rb
+++ simulator.rb
@@ -20,10 +20,16 @@ class Simulator
 
   def step 
     @nodes.each { |node| 
-      neighbourhood = node.step1 @nodes.find_all { |n| n != node}
+      neighbourhood = node.step1 @nodes.find_all { |n| n.node_id != node.node_id}
       puts "neighbourhood:"
       puts neighbourhood.inspect
       puts "--------------"
+
+      benefits_and_costs = { }
+      neighbourhood.each { |neighbour| 
+        benefits_and_costs[neighbour.node_id] = { :beta => 4, :gamma => 1 }
+      }
+      node.step2 benefits_and_costs
     }
   end
 
