9f2f5c79917bfc623a692bd165b17d0affa6cf09
Peter Lewis <p.r.lewis@cs.bham.ac.uk>
2012-01-31 15:01:58 +0000

    write results to files

--- sim_test.rb
+++ sim_test.rb
@@ -1,7 +1,8 @@
 require "./simulator"
 
 taus_file = File.open("sim.taus", 'w')
-utilities_file = File.open("sim.utilities", 'w')
+node_utilities_file = File.open("sim.node_utilities", 'w')
+conjoint_utilities_file = File.open("sim.conjoint_utilities", 'w')
 
 
 s = Simulator.new
@@ -19,11 +20,24 @@ end
 # Run a number of simulation steps.
 # Simulator.step can take a block. If one is passed in, then this is executed at
 # the end of each time step.
-10.times {
+10000.times do
   s.step do
-    s.nodes.find { |n| n.node_id==0 }.print_taus taus_file
+    # This block is executed at the end of each time step. It can be used for
+    # collecting data and printing it out, for example.
+
+    # We're only really interested in tracking one node, node 0
+    interesting_node = s.nodes.find { |n| n.node_id==0 }
+
+    # Print out the utility and tau associated with each possible node.
+    interesting_node.print_taus taus_file
+    interesting_node.print_total_utilities node_utilities_file
+
+    # Just print the cumulative conjoint utility so far
+    interesting_node.print_cumulative_conjoint_utility conjoint_utilities_file
   end
-}
+
+end
 
 taus_file.close
-utilities_file.close
+node_utilities_file.close
+conjoint_utilities_file.close
