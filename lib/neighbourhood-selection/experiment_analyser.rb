fa11ccbb65b57a29189239db0b465588126ac144
Peter Lewis <p.r.lewis@cs.bham.ac.uk>
2012-07-10 17:38:45 +0100

    add pairwise Wilcoxon Rank Sum tests

--- lib/neighbourhood-selection/experiment_analyser.rb
+++ lib/neighbourhood-selection/experiment_analyser.rb
@@ -128,24 +128,60 @@ class Experiment_Analyser
     @r.eval "matrix(means) -> means"
     @r.eval "matrix(sds) -> sds"
 
-    File.open(outfilename, "w") do |outfile|
-      outfile.puts "Final utility values for #{@dataset_name}, by variant:"
-      outfile.puts "---------------------------------------------------------"
-      outfile.puts "Variant".center(20," ") + "\t\t   Mean   \t   SD"
-      outfile.puts "---------------------------------------------------------"
-      @r.variants.each_with_index do |name, i|
-        outfile.puts name.center(20," ") + "\t\t" + "%0.3f" % @r.means[i,0] + "\t" + "%0.3f" % @r.sds[i,0]
+
+    # Now, find the variant with the highest mean.
+    best_variant = @r.variants.each_with_index.max_by { |name, i| @r.means[i,0] }[0]
+
+    # Next, calculate pairwise Wilcoxon Rank Sum test between the variant with
+    # the best mean and each other variant.
+    #
+    # Use data.final, which we created above, to compare samples of the final
+    # timestep's utility values.
+    p_values = {}
+    @r.variants.each_with_index do |comparator_variant, i|
+
+      unless comparator_variant == best_variant
+        @r.eval "a <- subset(data.final, subset=(Variant==\"#{best_variant}\"))"
+        @r.eval "b <- subset(data.final, subset=(Variant==\"#{comparator_variant}\"))"
+
+        @r.eval "result <- wilcox.test(a$Utility, b$Utility, correction=TRUE)"
+        @r.eval "p <- result$p.value"
+
+        p_values[comparator_variant] = @r.p
       end
-      outfile.puts "---------------------------------------------------------"
+
     end
 
-    #print "Means:    "
-    #@r.means.each { |name| print name + " " }
-    #p @r.means
 
-    #puts "SDs:"
-    #p @r.sds
-
+    # Finally, let's create some pretty output
+    File.open(outfilename, "w") do |outfile|
+      outfile.puts "Variant with best mean: #{best_variant}"
+      outfile.puts
+      outfile.puts "Final utility values for #{@dataset_name}, by variant:"
+      outfile.puts "-------------------------------------------------------------------"
+      outfile.puts "Variant".center(25," ") + "\t\t  Mean   \t   SD"
+      outfile.puts "-------------------------------------------------------------------"
+      @r.variants.each_with_index do |name, i|
+        outfile.puts name.center(25," ") + "\t\t" + "%0.3f" % @r.means[i,0] + "\t" + "%0.3f" % @r.sds[i,0]
+      end
+      outfile.puts "-------------------------------------------------------------------"
+      outfile.puts
+      outfile.puts "Wilcoxon Rank Sum test results. Comparing #{best_variant} pairwise with:"
+      outfile.puts "-------------------------------------------------------------------"
+      outfile.puts "Variant".center(25," ") + "\t\t p value\tSig Diff?"
+      outfile.puts "-------------------------------------------------------------------"
+      @r.variants.each_with_index do |name, i|
+        unless name == best_variant
+          outfile.print name.center(25," ") + "\t\t " + "%0.5f" % p_values[name]
+          if p_values[name] <= 0.05
+            outfile.puts "\t   *"
+          else
+            outfile.puts
+          end
+        end
+      end
+      outfile.puts "-------------------------------------------------------------------"
+    end
   end
 
 
