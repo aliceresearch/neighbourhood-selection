1229b21aa602e5bb8f6675cea6d7cf4853c53b55
Peter Lewis <p.r.lewis@cs.bham.ac.uk>
2012-07-11 13:21:33 +0100

    load data into experiment_set_analyser

--- lib/neighbourhood-selection/experiment_set_analyser.rb
+++ lib/neighbourhood-selection/experiment_set_analyser.rb
@@ -1,12 +1,38 @@
 require "rinruby"
 
+# TODO: DRY w.r.t experiment_analyser. Create common module or superclass?
+
 class Experiment_Set_Analyser
 
   def initialize datafiles, column_types
     @datafiles = datafiles
 
+    # Check we're not trying to analyse nothing
+    @datafiles.each do |datafile|
+      unless File.exists? datafile
+        R.quit
+        raise "Data file #{datafile} does not exist."
+      end
+    end
 
+    # Create an R instance for us to use
+    # We don't use the default one, since we want to set echo to off.
+    #
+    # TODO: Can I do this with R.echo(enable=false) ?
 
+    @r = RinRuby.new(:echo=>false)
+
+    # Convert the array of column types to an R friendly list string.
+    # TODO: Definitely a DRY candidate at the least!
+    column_types = column_types.to_s.gsub( /[\[\]]/, '').gsub( /"/, '\'')
+
+    # Load the data into the R session
+    @datafiles.each_with_index do |datafile, i|
+      print "Loading data file #{datafile} as data.#{i}..."
+      @r.eval "data.#{i} <- read.table( '#{datafile}', header = TRUE,
+                                    colClasses = c(#{column_types}))"
+      puts " done."
+    end
   end
 
 
